d509021b8d7ce443bc7e157c2e5816c97d24ec2f
donh <hudong@deepexi.com>
2020-11-04 15:26:16 +0800

    init

---
+++ src/main/java/com/example/drools/bean/Account.java
@@ -0,0 +1,22 @@
+package com.example.drools.bean;
+
+public class Account {
+    private String personName;
+    private Integer score;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
